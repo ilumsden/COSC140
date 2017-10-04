26e755d2323047b7122c2e89167db4b7bf035f26
Ian Lumsden <lumsden.ian@gmail.com>
2017-10-04 00:06:56 -0400

    Current version of Prime3.cpp

--- labs/lab3/Prime3.cpp
+++ labs/lab3/Prime3.cpp
@@ -1,18 +1,31 @@
-#include ...
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
 using namespace std;
 
 class isprime {
   public:
-    isprime() { ... }
+    isprime() {v.push_back(2);}
     bool operator()(int);
 
   private:
-    ...
+    vector<int> v;
+    void growlist(int);
 };
 
 bool isprime::operator()(int number)
 {
-    ...
+    if (number > v.back()) {
+        growlist(number);
+    }
+    bool exists = binary_search(v.begin(), v.end(), number);
+    if (exists) {
+        return true;
+    }
+    return false;
 }
 
 int random_number()
@@ -22,14 +35,18 @@ int random_number()
 
 int main(int argc, char *argv[])
 {
-	int N = ...
+	int N = 10;
+        if (argc == 2) {
+            //Add something here to assert that argv[1] is int
+            N = argv[1];
+        }
 
 	srand(N);
 
-	vector< ? > ...
-	vector< ? > ...
+	vector<int> num_list(N);
+	//vector< ? > ...
 
-	generate( ... );
+	generate(num_list.begin(), num_list.end(), random_number());
 	transform( ... );
 
 	int c = count( ... );
