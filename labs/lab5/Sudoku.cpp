35ba4912aeed78b452b1db85db294b445e95aeaa
ilumsden <lumsden.ian@gmail.com>
2017-10-18 12:00:22 -0400

    Current progress on this lab.

--- labs/lab5/Sudoku.cpp
+++ labs/lab5/Sudoku.cpp
@@ -19,9 +19,8 @@ class sudoku {
   private:
     int solve(arguments);
 
-    // various support functions 
-    // error_check_value()
-    // error_check_uniqueness()
+    bool error_check_value(int, int);
+    bool error_check_uniqueness();
 
     void display();
 
@@ -47,23 +46,91 @@ void sudoku::solve() {
   // exit if errors detected
 }
 
+bool sudoku::error_check_value(int min, int max) {
+    bool valid = true;
+	bool final_valid = true;
+	for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            valid = (game[i][j] >= min && game[i][j] <= max)?true:false;
+			if (valid == false) {
+				cerr << i << " " << j << " " << game[i][j] << " illegal value\n";
+				final_valid = false;
+			}
+		}
+	}
+	return final_valid;
+}
+
+bool sudoku::error_check_uniqueness() {
+	bool unique = true;
+	for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (game[i][j] == 0) {
+                continue;
+			}
+			else {
+                for (int k = 0; k < 9; k++) {
+                    if (game[i][k] == game[i][j] && k != j) {				    	
+                        cerr << row << " " << col << " " << val << " illegal non-unique value\n";
+						unique = false;
+					}
+					if (game[k][j] == game[i][j] && k != i) {
+                        cerr << row << " " << col << " " << val << " illegal non-unique value\n";
+						unique = false;
+					}
+				}
+	            int row_groupcheck = i - (i % 3);
+	            int col_groupcheck = j - (j % 3);
+				bool unique_cell = true;
+	            for (int l = row_groupcheck; l < row_groupcheck + 3; l++) {
+                    for (int m = col_groupcheck; m < col_groupcheck + 3; m++) {
+                        if (game[l][m] == game[i][j] && l != i && m != j) { 
+                            cerr << row << " " << col << " " << val << " illegal non-unique value\n";
+							unique = false;
+							unique_cell = false;
+							break;
+						}
+					}
+					if (!unique_cell) {
+                        break;
+					}
+				}
+			}
+		}
+	}
+	return unique;
+}
+
 void sudoku::read(const char *fname) {
   cout << "READ\n";
 
   ifstream fin(fname);
     
   int i, j, v;
+  bool kill = false;
 
   while (fin >> i >> j >> v) {
-    // error check grid indices
-    game[i][j] = v;
+    if (i < 0 || i > 8 || j < 0 || j > 8) {
+        cerr << i << " " << j << " " << v << " illegal grid index\n";
+		kill = true;
+	}
+	else {
+        game[i][j] = v;
+	}
   }
 
   fin.close();
 
-  // exit if bad grid indices
+  if (kill) {
+      exit(0);
+  }
 
-  // error check data values
+  bool valid_vals = error_check_value(0, 9);
+  if (!valid_vals) {
+      exit(0);
+  }
+
+  bool unique_vals = error_check_uniqueness
   // error check uniqueness
   // exit if errors detected
 }
