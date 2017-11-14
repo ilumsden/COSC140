2808ae0ebee6f92fb02fd9ea621094753d84f2f7
Ian Lumsden <lumsden.ian@gmail.com>
2017-11-13 19:09:20 -0500

    Adds comments to BST.h and adds the submission tar file for part 1.

--- labs/lab7/BST.h
+++ labs/lab7/BST.h
@@ -12,76 +12,97 @@ class bst {
     struct node {
         node(int ID = int());
 
-	    void print();
+	void print();
 
+        /* `key` is the data stored in the node.
+         * `id` is an int that states when the node was added to the tree.
+         * `parent` is a node pointer that points to the parent of this node.
+         * `link` is an array of node pointers. The pointers point to this
+         * node's children.
+         */
         TKey key;
-		int id;
+	int id;
         node *parent;
         node *link[2];
     };
   
     /*public:
-	    class iterator {
-	        public:
-		        //default constructor (no argument)
-		        //overloaded operators (++, *, ==, !=)
-	        private:
-	            //friend class bst<TKey>;
-		        //constructor (with argument)
-
-	            node *p;
-	    };
-
-	    //iterator begin() { ... }
-	    //iterator end() { ... }*/
+        class iterator {
+	    public:
+                //default constructor (no argument)
+		//overloaded operators (++, *, ==, !=)
+	    private:
+	        //friend class bst<TKey>;
+		//constructor (with argument)
+	        node *p;
+	};
+	//iterator begin() { ... }
+	//iterator end() { ... }*/
 
     public:
         bst() { Troot=NULL; tid=0;}
-	    ~bst() { clear(Troot); }
+	~bst() { clear(Troot); }
 
-	    bool empty() { return Troot==NULL; }
+	bool empty() { return Troot==NULL; }
 
-	    void insert(TKey &);
+	void insert(TKey &);
 
-	    //iterator lower_bound(const TKey &);
-	    //iterator upper_bound(const TKey &);
+	//iterator lower_bound(const TKey &);
+        //iterator upper_bound(const TKey &);
 
-	    void print_bylevel();
+	void print_bylevel();
 
     private:
-	    void clear(node *);
-	    node *insert(node *, TKey &);
+        void clear(node *);
+	node *insert(node *, TKey &);
         
-		// tid is a node ID used to pass the correct ID to a node that
-		// will be inserted.
-	    int tid;
+	// tid is a node ID used to pass the correct ID to a node that
+	// will be inserted.
+	int tid;
+        // Troot points to the root node of the tree.
         node *Troot;
 };
 
+/* This is the constructor for the node subclass.
+ * ID has a default value of 0 (int()).
+ * When run, it initializes `id` to the passed value (or default).
+ * Additionally, it sets the key to the default value of its data type,
+ * and it sets all the member pointers to NULL.
+ */
 template <class TKey>
 bst<TKey>::node::node(int ID) {
     id = ID;
     key = TKey();
-	parent = NULL;
+    parent = NULL;
     link[0] = NULL;
-	link[1] = NULL;
+    link[1] = NULL;
 }
 
+// This function prints the node's data.
 template <class TKey>
 void bst<TKey>::node::print() {
+    /* This cout statement prints out the node's
+     * key and id.
+     */
     cout << setw(3) << key << " "
-		 << setw(3) << id << ":";
-
-    //output node and parent ID information
-    //change below to output subtree ID information
+         << setw(3) << id << ":";
 	
-	if (id == 1) {
+    /* This if-else statement prints out "ROOT" if
+     * this node is the root node. Otherwise, it prints
+     * out the id of this node's parent node.
+     */
+    if (id == 1) {
         cout << " ROOT ";
-	}
-	else {
+    }
+    else {
         cout << " P=" << setw(3) << parent->id;
-	}
+    }
 
+    /* This block prints out the ids of the left
+     * and right children of this node. If the node
+     * doesn't have a child in the desired direction,
+     * "nothing" is printed out.
+     */
     if (link[0]) cout << " L=" << setw(3) << link[0]->id;
     else         cout << "      ";
     if (link[1]) cout << " R=" << setw(3) << link[1]->id;
@@ -107,20 +128,36 @@ void bst<TKey>::insert(TKey &key) {
     Troot = insert(Troot, key);
 }
 
+/* This function inserts a new node (with key = key parameter)
+ * into the tree. The only comments inside this function will
+ * be for the changes I made.
+ */
 template <class TKey>
 class bst<TKey>::node *bst<TKey>::insert(node *T, TKey &key) {
     if (T == NULL) {
-		tid++;
+        /* This part of the function has been edited to update
+         * `tid` and use it to initialize a new node with the correct
+         * id value. The desired key value is then set as the node's key.
+         */
+        tid++;
         T = new node(tid);
         T->key = key;
     } 
-	else if (T->key == key) {
+    else if (T->key == key) {
         ;
     } 
-	else {
+    else {
+        /* This part of the function will determine the direction
+         * that the traversal will need to take. Then, it recursively
+         * calls the insert function and uses its output to set the
+         * child node in direction `dir`. If the immediate child of `T`
+         * is not the inserted node, the recursion just relinks `T` and
+         * its previous child in direction `dir`. Finally, this block 
+         * links (or relinks) the child's parent and `T`.
+         */ 
         int dir = T->key < key;
         T->link[dir] = insert(T->link[dir], key);
-		T->link[dir]->parent = T;
+	T->link[dir]->parent = T;
     }
 
     return T;
